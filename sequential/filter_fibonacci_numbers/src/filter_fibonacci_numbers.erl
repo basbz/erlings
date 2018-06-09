3096d04ba277961aff8a3fa3532c4cc64e3fe029
Bas Ben Zineb <bas.benzineb@bynder.com>
2018-06-09 10:55:12 +0200

    Filter fibonacci_numbers

--- sequential/filter_fibonacci_numbers/src/filter_fibonacci_numbers.erl
+++ sequential/filter_fibonacci_numbers/src/filter_fibonacci_numbers.erl
@@ -2,5 +2,14 @@
 
 -export([filter/1]).
 
+is_perfect_square(X) ->
+ S = round(math:sqrt(X)),
+ S * S == X.
+
+
+is_fib(X) ->
+    Y = 5 * ( X * X),
+    is_perfect_square(Y + 4) or is_perfect_square(Y - 4).
+
 filter(List) ->
-  put_your_solution_here.
+    [X || X <- List, is_fib(X) == true].

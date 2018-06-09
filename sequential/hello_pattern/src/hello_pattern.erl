8f57ecc645ea30a5cd418b783e2f8604d0404219
Bas Ben Zineb <bas.benzineb@bynder.com>
2018-06-09 11:12:37 +0200

    Hello pattern matching!

--- sequential/hello_pattern/src/hello_pattern.erl
+++ sequential/hello_pattern/src/hello_pattern.erl
@@ -2,5 +2,14 @@
 
 -export([hello/1]).
 
-hello({tuple_element_1, tuple_element_2}) ->
-  resolve.
+hello({morning, _Person}) ->
+    morning;
+hello({night, _Person}) ->
+    night;
+hello({evening, Person}) ->
+    {good, evening, Person};
+hello({math_class, Number, _Subject}) when Number < 0 -> none;
+hello({math_class, _Number, Subject}) ->
+        {math_class, Subject};
+hello({_Time, _Person}) ->
+  ok.

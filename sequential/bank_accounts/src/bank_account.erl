d05809bfefc607acab78985c0b2b1490313625a8
Bas Ben Zineb <bas.benzineb@bynder.com>
2018-06-12 07:06:58 +0200

    Basic banking operations

--- sequential/bank_accounts/src/bank_account.erl
+++ sequential/bank_accounts/src/bank_account.erl
@@ -2,5 +2,24 @@
 
 -export([process_operation/2]).
 
-process_operation(Bank, Operation) ->
-  put_your_solution_here.
+account(List, Nr) ->
+    Rs = lists:filter(fun({N, _}) -> N == Nr end, List),
+    case Rs of
+        [] -> {error, account_not_found};
+        [Account] -> Account
+    end.
+
+process(withdraw, {Nr, Total}, Amount) ->
+    case Amount > Total of
+        true -> {error, insufficient_funds};
+        false -> {Nr, Total - Amount}
+    end;
+process(deposit, {Nr, Total}, Amount) ->
+        {Nr, Total + Amount}.
+
+process_operation(Bank, {Nr, Op, Amount}) ->
+    Account = account(Bank, Nr),
+    case Account  of
+        {error, account_not_found} -> Account;
+        _-> process(Op, Account, Amount)
+    end.
